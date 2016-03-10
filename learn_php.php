86bc8a07095e047e4512899a6153abe6676c2789
Michel Frechette <mifrec@hotmail.com>
2016-03-10 18:01:27 -0500

    *=

--- learn_php.php
+++ learn_php.php
@@ -21,11 +21,9 @@
 EOD;
 
       $randNum = 5;
-      echo ($randNum -= 7) . "<br>"; // outputs -2 to the screen
-      echo ($randNum -= 7) . "<br>"; // outputs -9 to the screen
-      echo ($randNum -= 7) . "<br>"; // outputs -16 to the screen
-      echo ($randNum -= 7) . "<br>"; // outputs -23 to the screen
-      // 5 - 7 = -2; -2 - 7 = -9; -9 - 7 = -16; -6 - 7 = -23
+      echo ($randNum *= 7) . "<br>"; // outputs 35 to the screen
+      echo ($randNum *= 7) . "<br>"; // outputs 245 to the screen
+      // 5 * 7 = 32; 35 * 7 = 245
      ?>
   </body>
 </html>
