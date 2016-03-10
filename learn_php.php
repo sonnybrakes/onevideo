840bd9265117dade2f669b3c64ea44063fa372d4
Michel Frechette <mifrec@hotmail.com>
2016-03-10 17:56:22 -0500

    +=

--- learn_php.php
+++ learn_php.php
@@ -21,28 +21,11 @@
 EOD;
 
       $randNum = 5;
-
-      echo ($randNum + 7) . "<br>"; // outputs 12 to the screen
-      echo ($randNum - 7) . "<br>"; // outputs -2 to the screen
-      echo ($randNum * 7) . "<br>"; // outputs 35 to the screen
-      echo ($randNum / 7) . "<br>"; // outputs 0.714285714286 to the screen?
-      echo ($randNum % 7) . "<br>"; // outputs 5 (remainder) to the screen
-
-// BROKEN CODE
-// in the following examples the $randNum is assigned a new value for each line of collator_get_error_code
-// not clear on how to fix this at the moment
       echo ($randNum += 7) . "<br>"; // outputs 12 to the screen
-      echo ($randNum -= 7) . "<br>"; // outputs 5 to the screen
-      echo ($randNum *= 7) . "<br>"; // outputs 35 to the screen
-      echo ($randNum /= 7) . "<br>"; // outputs 5 (needs explanation) to the screen?
-      echo ($randNum %= 7) . "<br>"; // outputs 5 (remainder) to the screen
-
-      echo ($randNum %= 7) . "<br>"; // outputs 5 (remainder) to the screen
-      echo ($randNum /= 7) . "<br>"; // outputs 0.714285714286 (needs explanation) to the screen?
-      echo ($randNum *= 7) . "<br>"; // outputs 5 to the screen
-      echo ($randNum -= 7) . "<br>"; // outputs -2 to the screen
-      echo ($randNum += 7) . "<br>"; // outputs 5 to the screen
-
+      echo ($randNum += 7) . "<br>"; // outputs 19 to the screen
+      echo ($randNum += 7) . "<br>"; // outputs 26 to the screen
+      echo ($randNum += 7) . "<br>"; // outputs 33 to the screen
+      // 7 + 5 = 12; 12 + 7 = 19; 19 + 7 = 26; 26 + 7 = 33
      ?>
   </body>
 </html>
