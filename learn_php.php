7089cb82ea2da9f87b9b5667bedb05b59f74d206
Michel Frechette <mifrec@hotmail.com>
2016-03-10 12:47:06 -0500

    echo + \n, double vs. single literal quotation

--- learn_php.php
+++ learn_php.php
@@ -5,7 +5,8 @@
   <body>
     <?php
 
-      echo '<h2>Data Processed</>\n'; // note the closing tag
+      echo "<h2>Data Processed</>\n"; // double quotation + \n vs.
+      echo '<h2>Data Processed</>\n'; // single quotation + \n 
 
      ?>
   </body>
