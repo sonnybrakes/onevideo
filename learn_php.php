956c1e3bb7102475675592de192d78f4f0c61f01
Michel Frechette <mifrec@hotmail.com>
2016-03-10 17:50:34 -0500

    lines 34 to 44 are not clearly understood at the moment

--- learn_php.php
+++ learn_php.php
@@ -20,10 +20,29 @@
       in $cityAddress</br>
 EOD;
 
-      echo $str;
+      $randNum = 5;
 
-      echo "<br> 5 + 2 = " . (string)(5 + 2); // outputs a string, not an integer
-      echo "<br> 5 / 2 = " . (double)(5 / 2) . "<br>"; // returns a floating point (decimal) number
+      echo ($randNum + 7) . "<br>"; // outputs 12 to the screen
+      echo ($randNum - 7) . "<br>"; // outputs -2 to the screen
+      echo ($randNum * 7) . "<br>"; // outputs 35 to the screen
+      echo ($randNum / 7) . "<br>"; // outputs 0.714285714286 to the screen?
+      echo ($randNum % 7) . "<br>"; // outputs 5 (remainder) to the screen
+
+// BROKEN CODE
+// in the following examples the $randNum is assigned a new value for each line of collator_get_error_code
+// not clear on how to fix this at the moment
+      echo ($randNum += 7) . "<br>"; // outputs 12 to the screen
+      echo ($randNum -= 7) . "<br>"; // outputs 5 to the screen
+      echo ($randNum *= 7) . "<br>"; // outputs 35 to the screen
+      echo ($randNum /= 7) . "<br>"; // outputs 5 (needs explanation) to the screen?
+      echo ($randNum %= 7) . "<br>"; // outputs 5 (remainder) to the screen
+
+      echo ($randNum %= 7) . "<br>"; // outputs 5 (remainder) to the screen
+      echo ($randNum /= 7) . "<br>"; // outputs 0.714285714286 (needs explanation) to the screen?
+      echo ($randNum *= 7) . "<br>"; // outputs 5 to the screen
+      echo ($randNum -= 7) . "<br>"; // outputs -2 to the screen
+      echo ($randNum += 7) . "<br>"; // outputs 5 to the screen
+
      ?>
   </body>
 </html>
