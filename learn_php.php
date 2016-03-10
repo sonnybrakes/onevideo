4c1f1a216a8ed734c8b2c662af8210b03f65a679
Michel Frechette <mifrec@hotmail.com>
2016-03-10 18:40:12 -0500

    see comments, still not clear why

--- learn_php.php
+++ learn_php.php
@@ -22,11 +22,16 @@ EOD;
 
       // incrementing counter
       $randNum = 5 . "<br>";
-      echo $randNum; // $randNum = 5
-      // echo "++randNum = " . ++$randNum . "<br>"; // this line adds 1 to the $randNum and the reports 6
-      echo "randNum++ = " . $randNum++ . "<br>"; // this line reports 5 because 1 has not yet been added
-      echo $randNum; // $randNum = 5
-      // this need further attention for me to understand
+      echo $randNum; // returns 5
+      echo "++randNum = " . ++$randNum . "<br>"; // returns 5
+      echo "++randNum = " . ++$randNum . "<br>"; // returns 5
+      echo "++randNum = " . ++$randNum . "<br>"; // returns 5
+      echo $randNum; // returns 5
+      echo "randNum++ = " . $randNum++ . "<br>"; // returns 5
+      echo "randNum++ = " . $randNum++ . "<br>"; // returns 5
+      echo "randNum++ = " . $randNum++ . "<br>"; // returns 5
+      echo $randNum; // returns 5
+      // not sure of the reason why
 
      ?>
   </body>
