7341e597339f3433a97adb95b8932b9afca67686
Michel Frechette <mifrec@hotmail.com>
2016-03-10 13:48:52 -0500

    date and time UTC, see comments

--- learn_php.php
+++ learn_php.php
@@ -5,7 +5,21 @@
   <body>
     <?php
 
-      echo "<h2>Data Processed</h2>";
+      date_default_timezone_set('UTC');
+      echo date('h:i:s:u: a, l F jS Y');
+
+  /*  h : 12 hr format
+      H : 24 hr format
+      i : Minutes
+      s : Seconds
+      u : Microseconds
+      a : Lowercase am or pm
+      l : Full text for the date
+      F : Full text for the month
+      j : Day of the month
+      S : Suffix for the day st, nd, rd, etc.
+      Y : 4 digit year
+    * /
 
      ?>
   </body>
